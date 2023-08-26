50c7f02fc02b208314512a4b48ce16167e09e1a4
zohir kioukiou <kiouzohir@gmail.com>
2023-08-26 14:28:53 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(books => renderBooks(books));
 }
 
 function renderBooks(books) {
@@ -13,5 +15,19 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks(renderBooks);
 });
+
+
+// function renderBooks(books) {
+//   const main = document.querySelector("main");
+//   books.forEach((book) => {
+//     const h2 = document.createElement("h2");
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   });
+// }
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   fetchBooks();
+// });
