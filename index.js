d7ddfb4a8d528a6a15fa055bb6c9b5fa66c0a4a6
zohir kioukiou <kiouzohir@gmail.com>
2023-08-26 14:29:31 +0100

    completed assginment

--- index.js
+++ index.js
@@ -19,15 +19,3 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-// function renderBooks(books) {
-//   const main = document.querySelector("main");
-//   books.forEach((book) => {
-//     const h2 = document.createElement("h2");
-//     h2.innerHTML = book.name;
-//     main.appendChild(h2);
-//   });
-// }
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   fetchBooks();
-// });
